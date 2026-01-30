4914ef8e2e8ece062a058894b8cc3ff1aa97c745
weekeeng <weekee.ng461@gmail.com>
2026-01-30 18:57:19 +0800

    updated api

--- app.js
+++ app.js
@@ -141,5 +141,5 @@ app.get('/logout', (req, res) => {
 });
 
 // ----------------- Start server -----------------
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
